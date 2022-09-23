d6597bfe088c0668caaa75410befc632e8c331ad
mbalabanov <marincomics@gmail.com>
2022-09-23 14:04:27 +0300

    Adds solutions as per slide deck

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
-import { useState } from "react"
-import {initialWorkouts, generateWorkout} from "./Workouts.js"
-import "./App.css"
+import { useState } from "react";
+import { initialWorkouts, generateWorkout } from "./Workouts.js";
+import "./App.css";
 
 function App() {
-  const [workouts, setWorkouts] = useState(initialWorkouts)
+  const [workouts, setWorkouts] = useState(initialWorkouts);
 
   const addNewWorkout = () => {
-    const newWorkout = generateWorkout()
-    console.log("addNewWorkout:", newWorkout)
-  }
+    const newWorkout = generateWorkout();
+    setWorkouts([...workouts, newWorkout]);
+  };
 
-  const deleteWorkout = (workout) => {
-    console.log("deleteWorkout:", workout)
-  }
+  const deleteWorkout = (workoutToDelete) => {
+    const reducedWorkouts = workouts.filter(function (workout) {
+      return workout !== workoutToDelete;
+    });
+    setWorkouts(reducedWorkouts);
+  };
 
-  const completeWorkout = (workout) => {
-    console.log("completeWorkout:", workout)
-  }
+  const completeWorkout = (workoutToUpdate) => {
+    console.log("workoutToUpdate", workoutToUpdate);
+
+    const updatedWorkouts = workouts.map(function (workout) {
+      if (workout === workoutToUpdate) {
+        return {
+          exercise: workoutToUpdate.exercise,
+          reps: workoutToUpdate.reps,
+          sets: workoutToUpdate.sets,
+          rest: workoutToUpdate.rest,
+          done: !workoutToUpdate.done,
+        };
+      }
+      return workout;
+    });
+
+    setWorkouts(updatedWorkouts);
+  };
 
   return (
     <div className="App">
@@ -26,19 +44,22 @@ function App() {
         {workouts.map((workout, index) => (
           <li key={index}>
             <p>
-              {workout.sets}x sets of <strong>{workout.reps}x{workout.exercise}</strong> with {workout.rest} seconds rest
+              {workout.sets}x sets of{" "}
+              <strong>
+                {workout.reps}x{workout.exercise}
+              </strong>{" "}
+              with {workout.rest} seconds rest
             </p>
-            {!workout.done && 
-              <button onClick={e=>completeWorkout(workout)}>Done</button>}
-            {workout.done && 
-             <p>✅</p>}
-            <button onClick={e=>deleteWorkout(workout)}>Delete</button>
+            {!workout.done && (
+              <button onClick={(e) => completeWorkout(workout)}>Done</button>
+            )}
+            {workout.done && <p>✅</p>}
+            <button onClick={(e) => deleteWorkout(workout)}>Delete</button>
           </li>
         ))}
       </ul>
-      
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Workouts.js
+++ src/Workouts.js
@@ -4,25 +4,25 @@ export const initialWorkouts = [
     reps: 10,
     sets: 4,
     rest: 5,
-    done: false
+    done: false,
   },
   {
     exercise: "Push Ups",
     reps: 10,
     rest: 10,
     sets: 20,
-    done: false
+    done: false,
   },
-]
+];
 
 const exercises = [
-  'Hill Sprints',
-  'Push Ups',
-  'Press Ups',
-  'Squat',
-  'Crunches',
-  'Burpees',
-]
+  "Hill Sprints",
+  "Push Ups",
+  "Press Ups",
+  "Squat",
+  "Crunches",
+  "Burpees",
+];
 
 export const generateWorkout = () => {
   return {
@@ -31,13 +31,13 @@ export const generateWorkout = () => {
     sets: getRandomInRange(1, 5),
     rest: getRandomInRange(10, 60),
     done: false,
-  }
-}
+  };
+};
 
 const getRandomItem = (items) => {
-  return items[Math.floor(Math.random() * items.length)]
-}
+  return items[Math.floor(Math.random() * items.length)];
+};
 
 const getRandomInRange = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1)) + min
-}
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
